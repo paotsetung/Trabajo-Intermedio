e993851025dc6d2f8b09e356c4d6c7175702e90e
paotsetung <caraxola@gmail.com>
2024-06-06 18:14:10 -0600

    Update juegoahorcado.py

--- juegoahorcado.py
+++ juegoahorcado.py
@@ -1,11 +1,5 @@
 import random
-print("Bienvenido al juego del ahorcado")
-def obtener_palabra_aleatoria():
-    lista_palabras = ["cadena", "imaginario", "arriba", "cantante", "mantenimiento", "experiencia", "local", "perro"]
-    palabra_aleatoria = random.choice(lista_palabras)
-
-import random
-print("Bienvenido al juego del Ahorcado!")
+print:("Bienvenido al juego del Ahorcado")
 def obtener_palabra_aleatoria():
     palabras = ["cadena", "imaginario", "arriba", "cantante", "mantenimiento", "experiencia", "local"]
     palabra_aleatoria = random.choice(palabras)
